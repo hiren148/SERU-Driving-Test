4ab7289c06dc487ef79676e629bc67670208a9cc
hiren <hirengujarati148@gmail.com>
2023-04-25 11:58:23 +0530

    revert app tracking transparency popup

--- lib/ui/screens/home/home.dart
+++ lib/ui/screens/home/home.dart
@@ -7,7 +7,6 @@ import 'package:driving_test/ui/screens/learn/learn.dart';
 import 'package:driving_test/ui/screens/profile/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -23,46 +22,6 @@ class _HomeScreenState extends State<HomeScreen> {
     ExamScreen(),
     ProfileScreen(),
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    initPlugin();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlugin() async {
-    final TrackingStatus status =
-        await AppTrackingTransparency.trackingAuthorizationStatus;
-    // If the system can show an authorization request dialog
-    if (status == TrackingStatus.notDetermined) {
-      // Show a custom explainer dialog before the system dialog
-      await showCustomTrackingDialog(context);
-      // Wait for dialog popping animation
-      await Future.delayed(const Duration(milliseconds: 200));
-      // Request system's tracking authorization dialog
-      await AppTrackingTransparency.requestTrackingAuthorization();
-    }
-  }
-
-  Future<void> showCustomTrackingDialog(BuildContext context) async =>
-      await showDialog<void>(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Dear User'),
-          content: const Text(
-            'We care about your privacy and data security. We keep this app free by showing ads. '
-            'Can we continue to use your data to tailor ads for you?\n\nYou can change your choice anytime in the app settings. '
-            'Our partners will collect data and use a unique identifier on your device to show you ads.',
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Continue'),
-            ),
-          ],
-        ),
-      );
 
   @override
   Widget build(BuildContext context) {
